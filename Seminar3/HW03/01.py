5fe1fe96bdf0cc62aa396153683f5bb760f87437
Romanov Aleksej <roman-off13@mail.ru>
2023-01-02 13:44:41 +0300

    01

--- Seminar3/HW03/01.py
+++ Seminar3/HW03/01.py
@@ -8,3 +8,23 @@
 # 22
 
 
+from random import sample
+
+def list_rand_nums(count: int) -> list:
+    if count < 0:
+        print("Ошибка! Число меньше нуля!")
+        return []
+
+    list_nums = sample(range(1, count * 2), count)
+    return list_nums
+
+def sum_odd_pos(list_nums: list):
+    sum_nums = 0
+    for k in range(0, len(list_nums), 2):
+        sum_nums += list_nums[k]
+    return sum_nums
+
+all_list = list_rand_nums(int(input("Введите число: ")))
+print(all_list)
+print(sum_odd_pos(all_list))
+
