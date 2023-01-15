32662ff48ccce322fd29eb6669bff19d5546ae1d
Romanov Aleksej <roman-off13@mail.ru>
2023-01-15 20:25:32 +0300

    03

---
+++ Seminar4/HW04/03.py
@@ -0,0 +1,53 @@
+# 3. Задайте последовательность чисел. 
+# Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности в том же порядке.
+# in
+# 7
+
+# out
+# [4, 5, 3, 3, 4, 1, 2]
+# [5, 1, 2]
+
+# in
+# -1
+
+# out
+# Negative value of the number of numbers!
+# []
+
+# in
+# 10
+
+# out
+# [4, 4, 5, 5, 6, 2, 3, 0, 9, 4]
+# [6, 2, 3, 0, 9]
+
+
+from random import randrange
+
+def list_rand_nums(count: int):
+    if count < 0:
+        print("Negative value of the number of numbers!")
+        return[]
+
+    list_nums = []
+    for i in range(count):
+        list_nums.append(randrange(count))
+
+    return list_nums
+
+def uniq_el(list_nums: list):
+    result = []
+    my_dict = {}.fromkeys(list_nums, 0)
+
+    for i in list_nums:
+        my_dict[i] += 1
+
+    for k in my_dict:
+        if my_dict[k] == 1:
+            result.append(k)
+
+        return result
+
+all_list = list_rand_nums(int(input("Number of numbers: ")))
+print(all_list)
+
